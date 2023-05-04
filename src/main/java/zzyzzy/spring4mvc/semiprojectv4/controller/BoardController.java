a2a2713d9b5a7946708ae2e26406b4e6aac5f299
hoteaklee <cc18ht@naver.com>
2023-05-04 16:45:30 +0900

    게사판 list 4 - 페이징 처리 3 - 페이지네이션 불럭처리

--- src/main/java/zzyzzy/spring4mvc/semiprojectv4/controller/BoardController.java
+++ src/main/java/zzyzzy/spring4mvc/semiprojectv4/controller/BoardController.java
@@ -17,8 +17,11 @@ public class BoardController {
     public ModelAndView list(int cpg) {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("board/list.tiles");
+
         mv.addObject("bdlist", bdsrv.readBoard(cpg));
         mv.addObject("cpg", cpg); /* 현재페이지 받아옴*/
+        // startPage = ((cpg - 1) / 10) * 10 + 1
+        mv.addObject("stpg", ((cpg - 1) / 10) * 10 + 1);
 
         return mv;
     }
